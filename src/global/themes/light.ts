1769966ff43a6a5cb92e419ba7904b952b6e31fa
Victor de Souto Soares <vitor.soares@lojainterativa.com>
2022-01-24 21:30:16 -0300

    chore: add initial setup

---
+++ src/global/themes/light.ts
@@ -0,0 +1,13 @@
+export default {
+  colors: {
+    primary: "#F553C2",
+    complemantary: "#A82880",
+    secondary: "#2AA818",
+
+    background: "#fff",
+    foreground: "#777777",
+
+    input: "#F9F9F9",
+    border: "rgba(0,0,0, .18)",
+  },
+};
